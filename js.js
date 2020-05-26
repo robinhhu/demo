1ae247cda7dd69b07f6377dce0c22ca2812c6f7b
Yitian Hu <emeraldhu@icloud.com>
2020-05-26 15:42:51 +0800

    final demo

---
+++ js.js
@@ -0,0 +1,15 @@
+$(document).ready (function () {
+  $(".sc").click( function() {
+    $(".sc1").css("opacity","1");
+  });
+  });
+
+  var button_my_button = "#alert1";
+  $(button_my_button).click(function(){
+   window.location.href='alertpic.html';
+  });
+
+  var but = "#alert2";
+  $(but).click(function(){
+   window.location.href='alertpic2.html';
+  });
